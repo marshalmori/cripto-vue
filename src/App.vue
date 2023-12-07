b00efd5f16c59aedb50accc9eec007f993c2241f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-06 22:39:35 -0300

    Organizar o template com componentes e composables

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, reactive } from "vue";
 import Alerta from "./components/Alerta.vue";
 import Spinner from "./components/Spinner.vue";
+import Cotizacion from "./components/Cotizacion.vue";
 import useCripto from "./composables/useCripto";
 
 const {
@@ -64,34 +65,7 @@ const cotizarCripto = () => {
       </form>
 
       <Spinner v-if="cargando" />
-
-      <div v-if="mostrarResultado" class="contenedor-resultado">
-        <h2>Cotización</h2>
-        <div class="resultado">
-          <img
-            :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL"
-            alt="imagen cripto"
-          />
-          <div>
-            <p>
-              El precio es de: <span>{{ cotizacion.PRICE }}</span>
-            </p>
-            <p>
-              Precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span>
-            </p>
-            <p>
-              Precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span>
-            </p>
-            <p>
-              Variación últimas 24 horas:
-              <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span>
-            </p>
-            <p>
-              Última Actualización: <span>{{ cotizacion.LASTUPDATE }}</span>
-            </p>
-          </div>
-        </div>
-      </div>
+      <Cotizacion v-if="mostrarResultado" :cotizacion="cotizacion" />
     </div>
   </div>
 </template>
