c03b43bf6c0e6742f5e487eac76599942ffa3c89
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-06 22:10:25 -0300

    Mover o código do App.vue para um composable

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <script setup>
-import { ref, reactive, onMounted, computed } from "vue";
+import { ref, reactive } from "vue";
 import Alerta from "./components/Alerta.vue";
 import Spinner from "./components/Spinner.vue";
 import useCripto from "./composables/useCripto";
 
-const {} = useCripto();
+const {
+  monedas,
+  criptomonedas,
+  cotizacion,
+  cargando,
+  obtenerCotizacion,
+  mostrarResultado,
+} = useCripto();
 
-const monedas = ref([
-  { codigo: "USD", texto: "Dolar de Estados Unidos" },
-  { codigo: "MXN", texto: "Peso Mexicano" },
-  { codigo: "EUR", texto: "Euro" },
-  { codigo: "GBP", texto: "Libra Esterlina" },
-]);
-const criptomonedas = ref([]);
 const error = ref("");
 
 const cotizar = reactive({
   moneda: "",
   criptomoneda: "",
-});
-
-const cotizacion = ref({});
-const cargando = ref(false);
-
-onMounted(() => {
-  const url =
-    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
-  fetch(url)
-    .then((respuesta) => respuesta.json())
-    .then(({ Data }) => (criptomonedas.value = Data));
 });
 
 const cotizarCripto = () => {
@@ -37,31 +26,8 @@ const cotizarCripto = () => {
     return;
   }
   error.value = "";
-  obtenerCotizacion();
+  obtenerCotizacion(cotizar);
 };
-
-const obtenerCotizacion = async () => {
-  cargando.value = true;
-  cotizacion.value = {};
-
-  try {
-    const { moneda, criptomoneda } = cotizar;
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
-
-    const respuesta = await fetch(url);
-    const data = await respuesta.json();
-
-    cotizacion.value = data.DISPLAY[criptomoneda][moneda];
-  } catch (error) {
-    console.log(error);
-  } finally {
-    cargando.value = false;
-  }
-};
-
-const mostrarResultado = computed(() => {
-  return Object.values(cotizacion.value).length > 0;
-});
 </script>
 
 <template>
